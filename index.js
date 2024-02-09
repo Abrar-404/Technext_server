e9df311a1281c499b68839f7d92972afffd0f579
Sahil Abrar <zeroxkhan2@gmail.com>
2024-02-10 00:33:49 +0600

    added user id get

--- index.js
+++ index.js
@@ -43,12 +43,12 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/users/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await usersCollection.findOne(query);
-      res.send(result);
-    });
+    // app.get('/users/:id', async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) };
+    //   const result = await usersCollection.findOne(query);
+    //   res.send(result);
+    // });
 
     app.post('/users', async (req, res) => {
       const data = req.body;
